3232d47804e2cf09806b95bdeaaf66d415b1694a
aahanad <150878761+aahanad@users.noreply.github.com>
2024-12-17 18:56:13 -0800

    Update main.py

--- main.py
+++ main.py
@@ -8,4 +8,10 @@ image=Image.open("christmas1.PNG")
 image=ImageTk.PhotoImage(image)
 imageLabel=Label(screen,image=image)
 imageLabel.place(relwidth=1,relheight=1)
+title=Label(screen,text="Jumbled word game  -christmas edition-",bg="plum1",fg="SpringGreen2",font=("Brush Script",19))
+title.place(x=6,y=100)
+good=["Santa Claus","elves", "Rudolph", "Jack Frost","carols","tree","gingerbread","stockings","presents","holly","tinsel","mistletoe",]
+bad=["talna saucs","eselv","olhrudp","cjka trfso","scoarl","eert","gairdbrnege","skingosct","retpness","lyohl","isntle","setiltemo"]
+guess=Entry()
+guess.place(x=190,y=230)
 screen.mainloop()
